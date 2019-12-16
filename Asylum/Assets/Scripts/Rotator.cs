273d64da4914e4939bf9895045ef479aa7db0646
Ari Punavuori <ari.punavuori@gmail.com>
2019-12-16 17:24:57 +0200

    Door rotating slowly intead of flipping

--- Asylum/Assets/Scripts/Rotator.cs
+++ Asylum/Assets/Scripts/Rotator.cs
@@ -5,19 +5,49 @@ using UnityEngine;
 public class Rotator : MonoBehaviour {
     GameObject world;
     GameObject rotator;
+    bool rotating;
+    float rotatingSpeed;
+    float targetYAngle;
+    Transform originalParent;
 
     private void Start() {
         world = GameObject.Find("World");
         rotator = GameObject.Find("Rotator");
     }
-    private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("Player")) {
-            // Move rotator to current portal, set world inside and rotate
-            // ToDo: Transition(s) instead of flip
-            rotator.transform.position = this.transform.position;
-            world.transform.SetParent(rotator.transform, true);
-            rotator.transform.Rotate(0, 180, 0);
-            world.transform.SetParent(null);
+
+    private void Update() {
+        if(rotating) {
+            Rotate();
+            if(rotator.transform.eulerAngles.y >= targetYAngle) {
+                StopRotating();
+            }
         }
+    }
+
+    public void RotateWorld() {
+
+        if(!rotating) {
+            print("Rotation started");
+            rotating = true;
+            originalParent = transform.parent;
+            rotator.transform.position = transform.position; // Set rotator to this objects position to rotate around
+            rotator.transform.rotation = Quaternion.identity;
+            rotatingSpeed = 100; // Set this to adjust speed of turn (Is set now to use buttons script to run RotateWorld())
+            targetYAngle = rotator.transform.eulerAngles.y + 180; // Same here (180)
+            world.transform.SetParent(rotator.transform, true);
+            transform.parent = null;
+        }
+    }
+
+    void Rotate() {
+        float newAngleDelta = Mathf.MoveTowardsAngle(rotator.transform.eulerAngles.y, targetYAngle, rotatingSpeed * Time.deltaTime);
+        rotator.transform.eulerAngles = new Vector3(0, newAngleDelta, 0);
+    }
+
+    void StopRotating() {
+        rotator.transform.eulerAngles = new Vector3(0, targetYAngle, 0);
+        world.transform.SetParent(null);
+        transform.parent = originalParent;
+        rotating = false;
     }
 }
